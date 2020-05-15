7f258464fb0c70c39644dc7cad4d651e8d347a90
Truchot <yanntruchot26@gmail.com>
2020-05-15 15:18:22 +0200

    Test 3-2

--- src/Adherent.php
+++ src/Adherent.php
@@ -5,10 +5,14 @@ class Adherent {
     private $identifiant;
 
     public function __construct($prenom, $nom, $date_de_naissance) {
-        $this->identifiant = self::concatenate($prenom, $nom, $date_de_naissance);
+        $this->identifiant = self::capitalisation($prenom, $nom, $date_de_naissance);
     } 
    
     public static function concatenate($prenom, $nom, $date_de_naissance) {
         return $prenom.$nom.$date_de_naissance->format('Y-m-d');
+    }
+
+    public static function capitalisation($prenom, $nom, $date_de_naissance) {
+        return strtoupper($prenom.$nom.$date_de_naissance->format('Y-m-d'));
     }
 }
