c33e37c342bf6bc3c3fea0970bd89ddd067a3028
Truchot <yanntruchot26@gmail.com>
2020-05-15 14:22:23 +0200

    Test 1-2

--- Tests/AdherentTest.php
+++ Tests/AdherentTest.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 final class AdherentTest extends TestCase {
-    public function testCanBeCreated()
-    {
+    
+    public function testCanBeCreated() {
         $this->assertInstanceOf(
             Adherent::class, 
             new Adherent('Yann','Truchot',new DateTime('1998-03-06'))
         );
     }
+
 }
--- src/Adherent.php
+++ src/Adherent.php
@@ -1,6 +1,11 @@
 <?php
 
 class Adherent {
-    private $identifiant;
     
+    private $identifiant;
+
+    public function __construct($prenom, $nom, $date_de_naissance) {
+        $this->identifiant = strtolower($prenom." ".$nom." ".$date_de_naissance->format('Y-m-d'));
+    } 
+   
 }
