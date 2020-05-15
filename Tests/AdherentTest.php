c9aec3c6a0f91e53bb64eb74abbc3024893479d8
Truchot <yanntruchot26@gmail.com>
2020-05-15 13:40:33 +0200

    Test 1-1

---
+++ Tests/AdherentTest.php
@@ -0,0 +1,14 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class AdherentTest extends TestCase {
+    public function testCanBeCreated()
+    {
+        $this->assertInstanceOf(
+            Adherent::class, 
+            new Adherent('Yann','Truchot',new DateTime('1998-03-06'))
+        );
+    }
+}
---
+++ src/Adherent.php
@@ -0,0 +1,6 @@
+<?php
+
+class Adherent {
+    private $identifiant;
+    
+}
