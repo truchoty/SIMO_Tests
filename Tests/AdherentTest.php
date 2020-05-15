589f9924dfbdf73262555833dc7582aa472f03e6
Truchot <yanntruchot26@gmail.com>
2020-05-15 15:06:23 +0200

    Test 2-1

--- Tests/AdherentTest.php
+++ Tests/AdherentTest.php
@@ -4,11 +4,20 @@ declare(strict_types=1);
 use PHPUnit\Framework\TestCase;
 
 final class AdherentTest extends TestCase {
-    
+
+    // Test 1
     public function testCanBeCreated() {
         $this->assertInstanceOf(
             Adherent::class, 
             new Adherent('Yann','Truchot',new DateTime('1998-03-06'))
+        );
+    }
+
+    // Test 2
+    public function testConcatenate() {
+        $this->assertEquals(
+            "YannTruchot1998-03-06",
+            Adherent::concatenate('Yann','Truchot',new DateTime('1998-03-06'))
         );
     }
 
