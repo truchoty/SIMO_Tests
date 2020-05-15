263bbc3bd72dac3a613ae20ebec4ce2c60b5c531
Truchot <yanntruchot26@gmail.com>
2020-05-15 15:28:25 +0200

    Test 4-2

--- src/Adherent.php
+++ src/Adherent.php
@@ -5,7 +5,7 @@ class Adherent {
     private $identifiant;
 
     public function __construct($prenom, $nom, $date_de_naissance) {
-        $this->identifiant = self::capitalisation($prenom, $nom, $date_de_naissance);
+        $this->identifiant = self::skip_accents(self::capitalisation($prenom, $nom, $date_de_naissance));
     } 
    
     public static function concatenate($prenom, $nom, $date_de_naissance) {
@@ -14,5 +14,16 @@ class Adherent {
 
     public static function capitalisation($prenom, $nom, $date_de_naissance) {
         return strtoupper($prenom.$nom.$date_de_naissance->format('Y-m-d'));
+    }
+
+    public static function skip_accents( $str, $charset='utf-8' ) {
+ 
+        $str = htmlentities( $str, ENT_NOQUOTES, $charset );
+        
+        $str = preg_replace( '#&([A-za-z])(?:acute|cedil|caron|circ|grave|orn|ring|slash|th|tilde|uml);#', '\1', $str );
+        $str = preg_replace( '#&([A-za-z]{2})(?:lig);#', '\1', $str );
+        $str = preg_replace( '#&[^;]+;#', '', $str );
+        
+        return $str;
     }
 }
