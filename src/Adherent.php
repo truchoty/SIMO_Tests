b0055668293967380d6792355ad0a9fc8de70e9f
Truchot <yanntruchot26@gmail.com>
2020-05-15 14:46:26 +0200

    Test 1-2 modif

--- src/Adherent.php
+++ src/Adherent.php
@@ -1,11 +1,11 @@
 <?php
 
 class Adherent {
-    
+
     private $identifiant;
 
     public function __construct($prenom, $nom, $date_de_naissance) {
-        $this->identifiant = strtolower($prenom." ".$nom." ".$date_de_naissance->format('Y-m-d'));
+        $this->identifiant = strtoupper($prenom.$nom.$date_de_naissance->format('Y-m-d'));
     } 
    
 }
